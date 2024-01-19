fe8c8c44f4ab5b9f6ce36d6ca8d8a72a510c7f58
i.stiaccini <i.stiaccini@frigel.com>
2024-01-19 14:54:20 +0100

    implemented up/dwn dictionary

--- client/gui/api_file_transfer_class.py
+++ client/gui/api_file_transfer_class.py
@@ -1,4 +1,57 @@
+from PyQt5.QtWidgets import QMainWindow
 from py.api_file_transfer import Ui_MainWindow
+import requests
 
-class Window:
-    pass
+# QPlainTextEdit
+# Widgets of interest:
+# self.ui.d_btn_dwn - dict
+# self.ui.d_btn_up
+# self.ui.d_txt
+# self.ui.f_btn_dwn - file
+# self.ui.f_btn_up
+# self.ui.df_btn_dwn - dict + file
+# self.ui.df_btn_up
+# self.ui.df_txt
+# self.ui.log_txt - log
+
+class MainWindow(QMainWindow):
+    LOCALHOST = 'http://127.0.0.1:5000'
+    DWN_D_URL = f"{LOCALHOST}/download_dict"
+    UP_D_URL = f"{LOCALHOST}/upload_dict"
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.connect_widgets()
+        self.show()
+
+    def connect_widgets(self):
+        self.ui.d_btn_dwn.clicked.connect(self.download_dict)
+        self.ui.d_btn_up.clicked.connect(self.upload_dict)
+        self.ui.f_btn_dwn.clicked.connect(self.download_file)
+        self.ui.f_btn_up.clicked.connect(self.upload_file)
+        self.ui.df_btn_dwn.clicked.connect(self.download_dict_file)
+        self.ui.df_btn_up.clicked.connect(self.upload_dict_file)
+    def download_dict(self):
+        req = requests.get(self.DWN_D_URL)
+        res = req.json()
+        self.ui.log_txt.setPlainText(res['message'])
+    def upload_dict(self):
+        text = self.ui.d_txt.toPlainText()
+        req = requests.post(self.UP_D_URL, json={"text": text})
+        res = req.json()
+        self.ui.log_txt.setPlainText(res['message'])
+
+    def upload_file(self):
+        pass
+
+    def download_file(self):
+        pass
+
+    def upload_dict_file(self):
+        pass
+
+    def download_dict_file(self):
+        pass
+
+
--- client/main.py
+++ client/main.py
@@ -1,2 +1,10 @@
-from gui.api_file_transfer_class import Window
+import sys, os
+sys.path.extend([os.path.join(os.path.dirname(os.path.abspath(__file__)), "gui")])
 
+from gui.api_file_transfer_class import MainWindow
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MainWindow()
+    sys.exit(app.exec_())
